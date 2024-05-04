38c51c6d692c3d777685224f0e327ebf30f590a5
AMSaiian <artursaiyan56@gmail.com>
2024-05-04 15:43:42 +0300

    Added missed file from previous commit

---
+++ src/First-App.Shared/InconsistentStateException.cs
@@ -0,0 +1,18 @@
+﻿namespace First_App.Shared;
+
+public class InconsistentStateException : Exception
+{
+    public InconsistentStateException()
+    {
+    }
+
+    public InconsistentStateException(string message)
+        : base(message)
+    {
+    }
+
+    public InconsistentStateException(string message, Exception inner)
+        : base(message, inner)
+    {
+    }
+}
