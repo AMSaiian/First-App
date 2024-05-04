9c644ca2d81259d5adaca157a70d23158c10649e
AMSaiian <artursaiyan56@gmail.com>
2024-05-04 17:28:25 +0300

    Added global exception handler

---
+++ src/First-App.WebApi/Middlewares/GlobalExceptionHandler.cs
@@ -0,0 +1,32 @@
+﻿using First_App.WebApi.Responses;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace First_App.WebApi.Middlewares;
+
+public class GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger) : IExceptionHandler
+{
+    private readonly ILogger<GlobalExceptionHandler> _logger = logger;
+
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext,
+                                                Exception exception,
+                                                CancellationToken cancellationToken)
+    {
+        _logger.LogError(exception.ToString());
+
+        ProblemDetails details = new()
+        {
+            Status = StatusCodes.Status500InternalServerError,
+            Title = ErrorTitles.ExceptionTitle,
+            Type = exception.GetType().Name,
+            Detail = exception.Message
+        };
+
+        httpContext.Response.StatusCode = details.Status.Value;
+
+        await httpContext.Response
+            .WriteAsJsonAsync(details, cancellationToken);
+
+        return true;
+    }
+}
